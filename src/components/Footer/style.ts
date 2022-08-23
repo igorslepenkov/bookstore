91789b57c1a1f43e33ed631c955c95a395db46e1
igorslepenkov <slepenkov.nii@yandex.by>
2022-08-23 19:17:34 +0300

    feat(details): improved styles for Footer

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,12 +1,17 @@
 import styled from "styled-components";
-import { fonts } from "../../ui";
+import { fonts, Media } from "../../ui";
 import { Color } from "../../ui";
 
 export const StyledFooter = styled.footer`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-between;
+  align-items: center;
   border-top: 1px solid ${Color.GreyLight};
+
+  ${Media.MD} {
+    flex-direction: row;
+  }
 `;
 
 export const FooterParagraph = styled.p`
