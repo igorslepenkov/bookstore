c148e33c14c04bd6f4e794c6b2fe5f716b52f777
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-12 14:04:44 +0300

    feat(favorites): add list of opular books to favorites page

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,10 +1,12 @@
-import { FavoritesCard, Page } from "../../components";
+import { FavoritesCard, Page, SimilarBooksList } from "../../components";
 import { Title } from "../../components";
+import { usePopularBooks } from "../../hooks";
 import { useGetFavorites } from "../../store";
 import { FavoritesList, NothingToShow } from "./style";
 
 export const FavoritesPage = () => {
   const favorites = useGetFavorites();
+  const popularBooks = usePopularBooks();
   return (
     <Page>
       <Title titleGrade={1} text="Favorites" />
@@ -17,6 +19,7 @@ export const FavoritesPage = () => {
       ) : (
         <NothingToShow>Nothing to show in favorites</NothingToShow>
       )}
+      {popularBooks && <SimilarBooksList similarBooks={popularBooks} />}
     </Page>
   );
 };
